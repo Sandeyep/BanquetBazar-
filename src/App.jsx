544d7f9e4c23e83af189b1c897c743ec3f2fc40a
Sandeyep <sandeyeppatel@gmail.com>
2025-12-26 00:16:49 +0545

    imported tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold "> React with Tailwind template for my final year project</h1>
+    </div>
+  )
+}
+
+export default App
